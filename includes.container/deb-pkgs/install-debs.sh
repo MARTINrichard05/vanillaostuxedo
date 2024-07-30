078f6042e259da8c954ec3ce269e5ba4e402786e
Richard <76814730+MARTINrichard05@users.noreply.github.com>
2024-07-30 11:53:12 +0200

    Update install-debs.sh

--- includes.container/deb-pkgs/install-debs.sh
+++ includes.container/deb-pkgs/install-debs.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 set -e
+
+# Install tuxedo-drivers package first
 for file in /deb-pkgs/*; do
-    if [ ! -f "$file" ]; then
+    if [! -f "$file" ]; then
         continue
     fi
 
     extension=${file##*.}
-    if [ $extension != "deb" ]; then
+    if [ $extension!= "deb" ]; then
+        continue
+    fi
+
+    filename=${file##*/}
+    if [[ $filename =~ ^tuxedo-drivers ]]; then
+        echo "installing $file"
+        apt-get install -y $file
+        continue 2
+    fi
+done
+
+# Install remaining packages
+for file in /deb-pkgs/*; do
+    if [! -f "$file" ]; then
+        continue
+    fi
+
+    extension=${file##*.}
+    if [ $extension!= "deb" ]; then
+        continue
+    fi
+
+    filename=${file##*/}
+    if [[ $filename =~ ^tuxedo-drivers ]]; then
         continue
     fi
 
     echo "installing $file"
-
     apt-get install -y $file
 done
